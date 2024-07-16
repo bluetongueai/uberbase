b94593d47d70f0d0e864bc90be6cb513839f77e1
Tim Gittos <tim.gittos@bluetongue.ai>
2024-07-16 13:00:18 +0000

    Change how params are passed to containers on run

--- functions/api/pkg/functions/client.go
+++ functions/api/pkg/functions/client.go
@@ -102,7 +102,8 @@ func (c client) Build(imageName, dockerfile string, context string) error {
 }
 
 func (c client) Run(imageName string, params ...string) (string, string, error) {
-	imageParams := append([]string{"run", "--rm", imageName}, params...)
+	paramString := string.Join(" ", params)
+	imageParams := append([]string{"run", "--rm", imageName}, paramString)
 	stdout, stderr, err := c.docker(imageParams...)
 	if err != nil {
 		log.Printf("failed to run image %s: %v", imageName, err)
