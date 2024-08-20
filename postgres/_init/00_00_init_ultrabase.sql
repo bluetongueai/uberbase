89784a24a5053a5ba935b85de050fcf937710a2f
Tim Gittos <tim.gittos@bluetongue.ai>
2024-08-20 17:41:51 +0000

    Switch logto for fusionauth - logto sucked in docker/behind a proxy

--- postgres/_init/00_00_init_ultrabase.sql
+++ postgres/_init/00_00_init_ultrabase.sql
@@ -1,9 +1,2 @@
 create role uberbase with password 'uberbase' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
-create role logto with password 'logto' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
 create role anon NOSUPERUSER LOGIN;
-
-create database logto with owner logto;
-
-\c logto
-grant all on schema public to postgres;
-grant all on schema public to logto;
