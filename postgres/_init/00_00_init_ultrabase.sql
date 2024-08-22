019c3a5099c5f62e25eb40f334054dd6a440fc83
Tim Gittos <tim.gittos@bluetongue.ai>
2024-08-21 19:31:08 -0500

    Changes from integration (#10)
    * Move Caddy out of compose stack, into its own run
    
    * Postgres and redis logging is back
    
    * All logging but Minio is back
    
    * Had to remove most logging - breaks when you mount vols thru
    
    * checkpoint
    
    * Pass args into dockerfile when building
    
    * Ensure env vars are overwritable
    
    * Switch logto for fusionauth - logto sucked in docker/behind a proxy
    
    * Fix logging finally?
    
    * Maybe maybe
    
    * Unbroke caddy
    
    * Fix seeding for fusionauth

--- postgres/_init/00_00_init_ultrabase.sql
+++ postgres/_init/00_00_init_ultrabase.sql
@@ -1,9 +1,4 @@
-create role uberbase with password 'uberbase' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
-create role logto with password 'logto' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
 create role anon NOSUPERUSER LOGIN;
 
-create database logto with owner logto;
-
-\c logto
-grant all on schema public to postgres;
-grant all on schema public to logto;
+create database uberbase with owner postgres encoding 'UTF8';
+create database fusionauth with owner postgres encoding 'UTF8';
