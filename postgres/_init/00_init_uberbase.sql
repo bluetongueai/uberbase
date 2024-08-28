9d6c3e1a7a6f41fa14caf5176de65a69cf0a9afc
Tim Gittos <tim.gittos@bluetongue.ai>
2024-08-28 12:32:57 +0000

    Non destructive postgres init config

--- postgres/_init/00_init_uberbase.sql
+++ postgres/_init/00_init_uberbase.sql
@@ -1,3 +1,12 @@
-create role anon NOSUPERUSER LOGIN;
+create role anon NOSUPERUSER NOLOGIN;
+create role admin NOSUPERUSER NOLOGIN;
+create role authenticated NOSUPERUSER NOLOGIN;
+create role authenticator noinherit login password 'mysecretpassword';
+
+grant anon to authenticator;
+grant admin to authenticator;
+grant authenticated to authenticator;
+
+grant all on schema public to admin;
 
 create database fusionauth with owner postgres encoding 'UTF8';
