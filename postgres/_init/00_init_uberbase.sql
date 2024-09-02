fc1c5aef33bff8baa742817bb791388191b8a240
Tim Gittos <tim.gittos@bluetongue.ai>
2024-09-02 21:14:35 +0000

    Fix fusionauth/postgres boot issue

--- postgres/_init/00_init_uberbase.sql
+++ postgres/_init/00_init_uberbase.sql
@@ -1,12 +1,8 @@
 create role anon NOSUPERUSER NOLOGIN;
 create role admin NOSUPERUSER NOLOGIN;
-create role authenticated NOSUPERUSER NOLOGIN;
 create role authenticator noinherit login password 'postgrest-secret-user-password';
 
 grant anon to authenticator;
 grant admin to authenticator;
-grant authenticated to authenticator;
-
-grant all on schema public to admin;
 
 create database fusionauth with owner postgres encoding 'UTF8';
